0f00088cec8fb27f4c775a924435a15c693376f0
kupsyloc <alexzlow@gmail.com>
2021-10-17 20:05:11 +0300

    update

--- headers/header.h
+++ headers/header.h
@@ -5,21 +5,56 @@
 #ifndef FT_PING_MY_HEADER_H
 #define FT_PING_MY_HEADER_H
 
-#include <stdbool.h>
 #include "stdio.h"
+#include <arpa/inet.h>
+#include <errno.h>
+#include <math.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/ip_icmp.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#define MSG_HELP "usage: ./ft_ping [-h] [-v] host"
-
+#define MSG_HELP "usage: ./ft_ping [-h help] [-v verbose] host"
+#define ERR_SOCKET "Error: Socket operation not permitted"
+#define ERR_SET_SOCKET_FLAG "Error: Cant resolve setsockopt"
+#define ERR_SET_SOCKET_SETTING "Error: Cant resolve setsockopt config"
 
 typedef struct s_ping_flags {
 	bool flag_v;
 	bool flag_h;
+	char flag_host[255];
 } t_ping_flags;
 
 typedef struct s_ping {
 	t_ping_flags flags;
+	unsigned int ttl;
+	unsigned int count;
+	pid_t process_id;
+	int socket_fd;
+	int sent_packets;
+	int received_packets;
+	int error_packets;
+	int seq;
+	struct sockaddr_in sockaddr;
+	struct timeval starting_time;
+	struct timeval ending_time;
+	double min_time;
+	double max_time;
+	double sum_time;
+	double square_sum_time;
+
+
 } t_ping;
 
 t_ping ping;
+
 
 #endif//FT_PING_MY_HEADER_H
--- src/main.c
+++ src/main.c
@@ -1,20 +1,68 @@
 #include "../headers/header.h"
 #include "../lib/includes/libftfull.h"
 
-//void parse
+void error_output_and_exit(char *message) {
+	fprintf(stderr, "%s\n", message);
+	exit(1);
+}
+
+void resolveDNS
 
 
+		char
+		parseFlags(int argc, char **argv) {
+	if (argc == 1) {
+		return 1;
+	}
+	for (int i = 1; i < argc; i++) {
+		if (ft_strequ(argv[i], "-h") != 0 || ft_strequ(argv[i], "-H") != 0) {
+			ping.flags.flag_h = true;
+			return 1;
+		} else if (ft_strequ(argv[i], "-v") != 0 || ft_strequ(argv[i], "-V") != 0) {
+			ping.flags.flag_v = true;
+		}
+	}
+	ft_strcpy(ping.flags.flag_host, argv[argc - 1]);
 
-void init(int argc, char **argv){
-	if (argc == 1){
-		printf(MSG_HELP);
+	ping.ttl = 64;
+	ping.process_id = getpid();
+	ping.seq = 1;
+	ping.socket_fd = -1;
+
+
+	return 0;
+}
+
+
+void createSocket() {
+	int socketFD;
+	struct timeval timeout;
+
+	timeout.tv_sec = 1;
+	timeout.tv_usec = 0;
+	if ((socketFD = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1) {
+		error_output_and_exit(ERR_SOCKET);
+	}
+	if ((setsockopt(socketFD, IPPROTO_IP, IP_TTL, &(ping.ttl), sizeof(ping.ttl))) == -1) {
+		error_output_and_exit(ERR_SET_SOCKET_FLAG);
+	}
+	if ((setsockopt(socketFD, SOL_SOCKET, SO_RCVTIMEO, (const void *) &timeout, sizeof(timeout))) == -1) {
+		error_output_and_exit(ERR_SET_SOCKET_SETTING);
 	}
 }
 
 
 int main(int argc, char **argv) {
-	init(argc, argv);
-	ping.flags.flag_h = true;
+	//	debug
+	t_ping *test = &ping;
+	//	end debug
+	printf("---- Start! ----\n");
+
+	if (parseFlags(argc, argv)) {
+		printf(MSG_HELP);
+		//	exit(0);
+	}
+	createSocket();
 	ft_printf("\nEnd!");
 	return 0;
 }
